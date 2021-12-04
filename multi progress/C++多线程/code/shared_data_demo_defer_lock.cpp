bd4860a652130127702741064723df3e0717b0bb
Bright <21725100@zju.edu.cn>
2021-12-04 23:10:43 +0800

    [multiprocess] Update code and notes

---
+++ multi progress/C++多线程/code/shared_data_demo_defer_lock.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <thread>
+#include <list>
+#include <vector>
+#include <mutex>
+
+class A
+{
+public:
+	// 把收到的消息（玩家命令）添加到一个队列
+	void inMsgRecQueue()
+	{
+		for (int i=0; i< 10000; ++i)
+		{
+			std::cout <<  "inMsgRecQueue 执行，插入一个消息 " <<std::endl;
+			
+			// unique lock 的 defer_lock 初始化一个没有加锁的 mutex 绑定到 unilock
+			std::unique_lock<std::mutex> unilock(myMut, std::defer_lock);
+			unilock.lock(); // 不需要关心 unlock
+
+			if(sbGuard1.owns_lock()){
+                    cout << "inMsgRecvQueue 执行，拿到锁头..." << endl;
+                    msgRecvQueue.push_back(i);
+			}
+            else{
+                    cout << "inMsgRecvQueue 执行，但是没有拿到锁头..." << endl;
+            }
+
+			msgRecQueue.push_back(i); // i 表示 玩家发送的消息
+
+			
+		}
+	}
+	
+	bool outMsgLULProc(int& command)
+	{
+		// unique lock 
+		std::unique_lock<std::mutex> unilock(myMut);
+		
+		std::chrono::milliseconds dura(2000);      // 20s
+        std::this_thread::sleep_for(dura);        // 线程休息20s 这回导致另外一个线程也会等待
+		
+		if (!msgRecQueue.empty())
+		{
+			// 消息队列不为空
+			command = msgRecQueue.front();
+			msgRecQueue.pop_front(); // 移除
+			
+			return true;
+		}
+
+		return false;
+	}
+	
+	void outMsgRecQueue()
+	{
+		int command = 0;
+		for (int i=0; i< 10000; ++i)
+		{
+			bool result = outMsgLULProc(command);
+			
+			if (result)
+			{
+				std::cout <<  "outMsgRecQueue 执行，取出一个元素 " <<std::endl;
+				// process data
+			}
+			else
+			{
+				// 消息队列为空
+				std::cout <<  "outMsgRecQueue 执行，但消息队列为空 " <<std::endl;
+			}
+		}
+		
+		std::cout << "end" << std::endl;
+	}
+	
+private:
+	std::list<int> msgRecQueue; // 容器，专门用于代表玩家给服务器发送的命令
+	std::mutex myMut;
+};
+
+int main()
+{
+	A a;
+	std::thread tin(&A::inMsgRecQueue, &a);
+	std::thread tout(&A::outMsgRecQueue, &a);
+	
+	tin.join();
+	tout.join();
+	
+	return 0;
+}
---
+++ multi progress/C++多线程/code/shared_data_demo_unique_lock_1.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <thread>
+#include <list>
+#include <vector>
+#include <mutex>
+
+class A
+{
+public:
+	// 把收到的消息（玩家命令）添加到一个队列
+	void inMsgRecQueue()
+	{
+		for (int i=0; i< 10000; ++i)
+		{
+			std::cout <<  "inMsgRecQueue 执行，插入一个消息 " <<std::endl;
+			
+			// unique lock 
+			unique_lock<std::mutex> ulock(myMut);
+			// myMut.lock();
+			msgRecQueue.push_back(i); // i 表示 玩家发送的消息
+			// myMut.unlock();
+			
+		}
+	}
+	
+	bool outMsgLULProc(int& command)
+	{
+		// unique lock 
+		unique_lock<std::mutex> ulock(myMut);
+		// myMut.lock();
+		if (!msgRecQueue.empty())
+		{
+			// 消息队列不为空
+			command = msgRecQueue.front();
+			msgRecQueue.pop_front(); // 移除
+			
+			return true;
+		}
+
+		return false;
+	}
+	
+	void outMsgRecQueue()
+	{
+		int command = 0;
+		for (int i=0; i< 10000; ++i)
+		{
+			bool result = outMsgLULProc(command);
+			
+			if (result)
+			{
+				std::cout <<  "outMsgRecQueue 执行，取出一个元素 " <<std::endl;
+				// process data
+			}
+			else
+			{
+				// 消息队列为空
+				std::cout <<  "outMsgRecQueue 执行，但消息队列为空 " <<std::endl;
+			}
+		}
+		
+		std::cout << "end" << std::endl;
+	}
+	
+private:
+	std::list<int> msgRecQueue; // 容器，专门用于代表玩家给服务器发送的命令
+	std::mutex myMut;
+};
+
+int main()
+{
+	A a;
+	std::thread tin(&A::inMsgRecQueue, &a);
+	std::thread tout(&A::outMsgRecQueue, &a);
+	
+	tin.join();
+	tout.join();
+	
+	return 0;
+}
